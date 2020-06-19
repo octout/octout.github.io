8f4588069cb00db8efce71bd584cb317cecfe426
octout <octout@users.noreply.github.com>
2020-06-20 00:15:50 +0900

    Update values_wdc.js

--- values_wdc.js
+++ values_wdc.js
@@ -61,13 +61,12 @@
         str_apikey = args.apikey;
         $.getJSON("Obs.json", function(json) { //ローカルのjsonへアクセス
             for (var i = 0, len = json.length; i < len; i++) {
-                tableData = [];
                 dateString = "query=" + json[i].lat + "," + json[i].lon,
                 apiCall = "https://atlas.microsoft.com/weather/forecast/hourly/json?subscription-key=" + str_apikey +"&api-version=1.0&" + dateString + "&duration=72&language=ja";
                 (function(t){
                     $.getJSON(apiCall, function(resp) {                  
                         var forecast = resp.forecasts,
-
+                        tableData = [];
                         // Iterate over the JSON object
                         for(var j = 0, len = forecast.length; j < len; j++) {
                             tableData.push({
