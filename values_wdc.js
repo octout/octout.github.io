0fcaf1bbc75200b9c24ef52f354de87223c50a43
octout <octout@users.noreply.github.com>
2020-06-21 14:19:37 +0900

    Update values_wdc.js

--- values_wdc.js
+++ values_wdc.js
@@ -59,18 +59,19 @@
     myConnector.getData = function(table, doneCallback) {
         var args = JSON.parse(tableau.connectionData);
         str_apikey = args.apikey;
-        var tableData = [];
         $.getJSON("Obs.json", function(json) { //ローカルのjsonへアクセス
             for (var i = 0, len = json.length; i < len; i++) {
                 dateString = "query=" + json[i].lat + "," + json[i].lon,
                 apiCall = "https://atlas.microsoft.com/weather/forecast/hourly/json?subscription-key=" + str_apikey +"&api-version=1.0&" + dateString + "&duration=72&language=ja";
+                (function(t){
                     $.getJSON(apiCall, function(resp) {                  
                         var forecast = resp.forecasts,
+                            tableData = [];
                         // Iterate over the JSON object
                         for(var j = 0, len = forecast.length; j < len; j++) {
                             tableData.push({
-                                "Obs_id":json[Math.floor(j/72)].Obs_id,
-                                "Obs_name":json[Math.floor(j/72)].Obs_name,
+                                "Obs_id":json[t].Obs_id,
+                                "Obs_name":json[t].Obs_name,
                                 "date":forecast[j].date,
                                 "weather":forecast[j].iconPhrase,
                                 "temp":forecast[j].temperature.value,
@@ -85,11 +86,14 @@
                                 "windgustspeed":forecast[i].windGust.speed.value
                             });
                         }
+                    table.appendRows(tableData);
+                    doneCallback();
                     });
+                })(i);
+
             }
+     
         });
-        table.appendRows(tableData);
-        doneCallback();
     };
 
     tableau.registerConnector(myConnector);
